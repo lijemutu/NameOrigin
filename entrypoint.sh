02715632ff72adfbc82a80d5ec88758c182606df
lijemutu <erlowarlo@gmail.com>
2022-08-17 15:31:49 -0500

    modify entrypoint to debug better

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+echo "------flask init-----"
 flask db init
+echo "------flask migrate-----"
 flask db migrate
+echo "------flask upgrade-----"
 flask db upgrade
 # flask run --host=0.0.0.0
+echo "------gunicorn-----"
 gunicorn -b 0.0.0.0:5000 run:app
